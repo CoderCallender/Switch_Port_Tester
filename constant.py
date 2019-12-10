60bde30e5b5ce55a68d591dd05602bb9e35b97b2
CoderCallender <42542281+CoderCallender@users.noreply.github.com>
2019-12-10 16:05:53 +0000

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,17 @@
+from gpiozero import LED
+from time import sleep
+
+SwitchOne = LED(4)
+SwitchTwo = LED(14)
+i = 0
+
+print ("Starting...")
+
+while true:
+    SwitchOne.on()
+    SwitchTwo.on()
+    sleep(0.1)
+    SwitchOne.off()
+    SwitchTwo.off()
+    sleep(0.1)
+    
